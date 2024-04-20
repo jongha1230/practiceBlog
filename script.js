5517a4a17e61dd61d64836bd4ba91160ad446a2d
Jongha <jongha1230@naver.com>
2024-04-20 17:11:52 +0900

    블로그쓰기 폼js 작성

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 페이지가 로드될 때 '홈' 메뉴를 활성화합니다.
+    // 페이지가 로드될 때 '홈' 메뉴를 활성화
     switchTab('home');
 });
 
 // .header-list 클릭된 상태 스타일 처리
 function switchTab(menuId) {
-    // 모든 메뉴 항목에 'clicked' 클래스를 제거합니다.
-    var menuItems = document.querySelectorAll('.header-list li');
+    // 모든 메뉴 항목에 'clicked' 클래스를 제거
+    const menuItems = document.querySelectorAll('.header-list li');
     menuItems.forEach(function(item) {
         item.classList.remove('clicked');
     });
 
-    // 클릭된 메뉴 항목에 'clicked' 클래스를 추가합니다.
-    var clickedItem = document.getElementById(menuId);
+    // 클릭된 메뉴 항목에 'clicked' 클래스를 추가
+    const clickedItem = document.getElementById(menuId);
     clickedItem.classList.add('clicked');
 
-    // 선택된 섹션을 표시하고, 나머지 섹션은 숨깁니다.
-    var sections = document.querySelectorAll('.post-section');
+    // 선택된 섹션을 표시하고, 나머지 섹션은 숨기기
+    const sections = document.querySelectorAll('.post-section');
     sections.forEach(function(section) {
         section.style.display = 'none';
     });
-    var selectedSection = document.getElementById(menuId + 'Content');
+    const selectedSection = document.getElementById(menuId + 'Content');
     selectedSection.style.display = 'block';
 }
 
