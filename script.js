eff65bcfbcc5aaa3a24647a01cde0a3bfeac85f3
Jongha <jongha1230@naver.com>
2024-04-20 17:01:32 +0900

    index.html 파일 수정

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     // 페이지가 로드될 때 '홈' 메뉴를 활성화합니다.
-    toggleMenu('home');
+    switchTab('home');
 });
 
 // .header-list 클릭된 상태 스타일 처리
-function toggleMenu(menuId) {
+function switchTab(menuId) {
     // 모든 메뉴 항목에 'clicked' 클래스를 제거합니다.
     var menuItems = document.querySelectorAll('.header-list li');
     menuItems.forEach(function(item) {
